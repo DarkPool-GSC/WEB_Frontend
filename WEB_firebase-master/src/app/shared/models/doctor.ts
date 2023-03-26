ef7022d0817f1fd564eeab641723889bb0f365be
adhishrayas <96647579+adhishrayas@users.noreply.github.com>
2023-03-26 22:23:16 +0530

    cleaned up code

--- WEB_firebase-master/src/app/shared/models/doctor.ts
+++
@@ -1,9 +0,0 @@
-
-export interface Doctor {
-    uid: string
-    Doctor_name: string
-    Doctor_photourl: string
-    Doctor_Qualification: string
-    Doctor_Experience: number
-    Doctor_specialization: number
-}
