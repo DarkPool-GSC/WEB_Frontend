97d80ee3d9955b2d82e7a35f012ca1bafb030501
harshitabhatia26 <bharshita2604@gmail.com>
2023-03-28 00:53:55 +0530

    Integrated patient data to dashboard frontend

--- src/app/Components/receptionist-dashboard/list/list.component.ts
+++ src/app/Components/receptionist-dashboard/list/list.component.ts
@@ -1,42 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { PatientService } from 'src/app/shared/services/hospital/patient.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit{
-  transactions = [
-    {
-      id: 1,
-      title: "Adhishraya Sharma",
-      price: "12345",
-      shop: "Tech Pro",
-      location: "Diabetes",
-      status: "In-Treatment",
-      imgSrc: "https://spec.nith.ac.in/Team%20Page/assets/team_images/Adhishraya.jpg"
-    },
-    {
-      id: 2,
-      title: "Adhishraya Sharma",
-      price: "23456",
-      shop: "Pick the best",
-      location: "Diabetes",
-      status: "In-Treatment",
-      imgSrc: "https://spec.nith.ac.in/Team%20Page/assets/team_images/Adhishraya.jpg"
-    },
-    {
-      id: 3,
-      title: "Adhishraya Sharma",
-      price: "34567",
-      shop: "Quality Leathers",
-      location: "Diabetes",
-      status: "In-Treatment",
-      imgSrc: "https://spec.nith.ac.in/Team%20Page/assets/team_images/Adhishraya.jpg"
-    }
-  ];
-  constructor() { }
-
-  ngOnInit(): void {
+export class ListComponent {
+  arr:any;
+  constructor(public patientservice: PatientService) { 
+    this.GetAllPatients();
+  }
+  async GetAllPatients (){
+    this.arr = await this.patientservice.GetAllPatients();
   }
 }
