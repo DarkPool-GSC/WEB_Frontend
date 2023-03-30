3a6feb40008ff2f7e653f2665d1ebbd70e7acf20
adhishrayas <96647579+adhishrayas@users.noreply.github.com>
2023-03-31 00:13:19 +0530

    Linked data to app

--- src/app/shared/models/patient.ts
+++ src/app/shared/models/patient.ts
@@ -2,9 +2,9 @@ export interface Patient {
     uid: string;
     display_name: string;
     image_fieldurl: string;
-    Age: number;
-    Phone_No: number;
-    Ailments: string;
+    age: number;
+    phone_No: number;
+    ailments: string;
     Notes: string[];
     Medication_name: string;
     Medication_Dose: number;
